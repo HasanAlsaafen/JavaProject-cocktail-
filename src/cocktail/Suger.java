f27df24c0fafcefa546d3352f06ae577c0c45bc1
HasanAlsaafen <hasansaafen1234@gmail.com>
2024-04-23 14:02:09 +0300

    the first stage

---
+++ src/cocktail/Suger.java
@@ -0,0 +1,24 @@
+
+package cocktail;
+
+
+public class Suger extends Ingredients {
+
+    public Suger(String name, double calories) {
+        super(name, calories);
+    }
+
+    public Suger() {
+    }
+
+    
+   
+    @Override
+    public String getInfo() {
+        return super.getCalories()+ "calories";
+    }
+
+   
+  
+    
+}
